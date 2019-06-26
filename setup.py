391be9ea9810affa550d11b3a604e4e2d29738af
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 01:45:34 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-command',
+    version='1.0',
+    description='A highlevel interface for command execution',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-command',
+    packages=['command'])
